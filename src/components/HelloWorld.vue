8fb94dbc04b193e71a8eb73590b8545d1c3ce457
Tsunemasa Hachiya <tsunemasa.hachiya@crowdworks.co.jp>
2018-09-26 10:06:40 +0900

    Fixed by ESLint instructions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,12 +6,11 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+	name: 'HelloWorld',
+	data() {
+		return {
+			msg: 'Welcome to Your Vue.js App',
+		}
+	},
 }
 </script>
-
